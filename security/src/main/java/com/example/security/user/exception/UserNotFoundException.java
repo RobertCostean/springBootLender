d3eda0cbfe1549e3605ed036e03487cd5b284e05
RobertCostean <66574496+RobertCostean@users.noreply.github.com>
2022-12-28 05:58:49 +0200

    Delete security directory

--- security/src/main/java/com/example/security/user/exception/UserNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.example.security.user.exception;
-
-public class UserNotFoundException extends RuntimeException{
-}
--- security/src/main/java/com/example/security/user/model/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.security.user.model.repository;
-
-import com.example.security.user.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, String> {
-
-}
